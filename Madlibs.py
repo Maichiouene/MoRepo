6781f229da95b887d2db07bec83ef82a096f5a79
Mohamed-El-Amine Aichiouene <u113168@Global.Bio-rad.com>
2022-07-11 13:03:15 -0600

    First Commit in Python Tingz

---
+++ Madlibs.py
@@ -0,0 +1,13 @@
+##### Let's start by printing a string that says you want to subscribe to a youtuber
+
+#Youtuber="Hochelaga"
+#print(f'Subscribe to {Youtuber}')
+
+adj=input('Adjective:')
+adj2=input('Adjective:')
+noun=input('Noun: ')
+
+madlib=f"The weather today has made me feel very {adj}. These clouds are awfully {adj2}. \
+     Maybe we should head in and play a game of {noun}." 
+ 
+print(madlib)
---
+++ Scratch.py
@@ -0,0 +1,13 @@
+import string
+
+
+alphabet=set(string.ascii_uppercase)
+
+user=input('Give me a letter').upper()
+
+used=['B','C']
+
+
+#if (user in alphabet - used):
+#    print('ok')
+
